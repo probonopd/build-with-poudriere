1dd64955f29fe25d8760093a58939d9f469a08eb
probonopd <probonopd@users.noreply.github.com>
2021-10-28 19:12:38 +0200

    Create porttest.sh

---
+++ porttest.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+PORT=www/chromium
+
+PORTSDIR=/usr/ports
+
+set -ex
+
+pwd
+
+cd /usr
+mv ports ports.old || true
+git clone --depth=1 --single-branch -b main https://github.com/freebsd/freebsd-ports.git ports
+
+cd ${CIRRUS_WORKING_DIR}
+for p in `cat list.txt`
+do
+	if [ -d ${PORTSDIR}/${p}/ ]; then
+		rm -fr ${PORTSDIR}/${p}/*
+	else
+		mkdir -p ${PORTSDIR}/${p}
+	fi
+        cp -R ./${p}/* ${PORTSDIR}/${p}/
+done
+
+mkdir /usr/ports/distfiles
+
+df -h
+
+echo "NO_ZFS=yes" >> /usr/local/etc/poudriere.conf
+echo "ALLOW_MAKE_JOBS=yes" >> /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+mkdir -p /usr/local/poudriere
+
+poudriere jail -c -j jail -v `uname -r`
+poudriere ports -c -f none -m null -M /usr/ports
+
+set +e
+poudriere testport -b latest -j jail ${PORT}
+RESULT=$?
+if [ ${RESULT} -eq 0 ]; then
+	exit 0
+fi
+set -e
+
+ls -l /usr/local/poudriere/data/logs/bulk/jail-default/latest/logs/errors
+for i in /usr/local/poudriere/data/logs/bulk/jail-default/latest/logs/errors/*.log
+do
+	echo ==== $i ====
+	cat $i
+done
+
+exit ${RESULT}
