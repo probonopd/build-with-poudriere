1f7992e3c0b54555da1a6d876fe237f832686991
probonopd <probonopd@users.noreply.github.com>
2021-11-01 08:41:50 +0100

    port.sh [ci skip]

---
+++ port.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+PORT=www/chromium
+
+PORTSDIR=/usr/ports
+
+set -ex
+
+pwd
+
+cd /usr
+mv ports ports.old || true
+git clone --depth=1 --single-branch -b main https://github.com/freebsd/freebsd-ports.git ports
+
+cd ${CIRRUS_WORKING_DIR}
+for p in `cat list.txt`
+do
+	if [ -d ${PORTSDIR}/${p}/ ]; then
+		rm -fr ${PORTSDIR}/${p}/*
+	else
+		mkdir -p ${PORTSDIR}/${p}
+	fi
+        cp -R ./${p}/* ${PORTSDIR}/${p}/
+done
+
+mkdir /usr/ports/distfiles
+
+df -h
+
+set +e
+make all-depends-list | cut -c 12- | xargs pkg install -y # Install all dependencies from packages
+cd /usr/ports/${PORT}
+MAKE_JOBS_UNSAFE=yes make -j8
+RESULT=$?
+if [ ${RESULT} -eq 0 ]; then
+	exit 0
+fi
+set -e
+
+#TODO: Show log
+#find /var/log/synth
+#for i in /usr/local/poudriere/data/logs/bulk/jail-default/latest/logs/errors/*.log # FIXME: Use the above
+#do
+#	echo ==== $i ====
+#	cat $i
+#done
+
+exit ${RESULT}
